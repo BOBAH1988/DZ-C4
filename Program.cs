8b34c59e1a30888ba092da0b13725ea15e4a32ac
BOBAH1988 <79042044926@ya.ru>
2023-03-08 17:08:53 +0300

    finish dz4

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
+
+// for (int i = 1; i < b; i++)
+// {
+// step = step * a;
+// }
+// Console.WriteLine("A в степени B равно: " + step);
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while(a > 0)
+// {
+//     int num = a % 10;
+//     a = a / 10;
+//     sum = sum + num;
+// }
+
+// Console.WriteLine(sum);
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int lenArray = ReadInt("Введите длинну массива: ");
+
+// int[] randomArray = new int[lenArray];
+// for (int i = 0; i < randomArray.Length; i++)
+// {
+//     randomArray[i] = new Random().Next(1,9);
+//     Console.Write(randomArray[i] + " ");
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
